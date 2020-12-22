715310c189bd52b7612b39603059980735221514
Alex Munger <munger.alex@gmail.com>
2020-12-22 16:00:03 -0500

    tex updates

--- EloXL.py
+++ EloXL.py
@@ -3,18 +3,19 @@ import xlwings as xw
 
 def main():
     wb = xw.Book.caller()
-    sheet = wb.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
+    sheet = wb.sheets['Sandbox']
+    if sheet['A1'].value == 'Hello XLWings!':
+        sheet['A1'].value = 'Bye XLWings!'
     else:
-        sheet["A1"].value = "Hello xlwings!"
+        sheet['A1'].value = 'Hello XLWings!'
+    sheet.autofit()
 
 
 @xw.func
 def hello(name):
-    return f"Hello {name}!"
+    return f'Hello {name}!'
 
 
-if __name__ == "__main__":
-    xw.Book("EloXL.xlsm").set_mock_caller()
+if __name__ == '__main__':
+    xw.Book('EloXL.xlsm').set_mock_caller()
     main()
