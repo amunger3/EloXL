296873851997d8e08d2b4c3098981de395fc42bf
Alex Munger <munger.alex@gmail.com>
2020-12-22 18:05:04 -0500

    witre eloNows

--- EloXL.py
+++ EloXL.py
@@ -1,14 +1,54 @@
 import xlwings as xw
+import numpy as np
+import pandas as pd
+
+import json
 
 
-def main():
-    wb = xw.Book.caller()
-    sheet = wb.sheets['Sandbox']
-    if sheet['A1'].value == 'Hello XLWings!':
-        sheet['A1'].value = 'Bye XLWings!'
-    else:
-        sheet['A1'].value = 'Hello XLWings!'
-    sheet.autofit()
+class ReadJSON:
+
+    def __init__(self):
+        self.file = 'fbd.json'
+        with open(self.file, encoding='utf-8') as json_file:
+            self.data = json.load(json_file)
+        self.wb = xw.Book.caller()
+
+    def write_leagues(self):
+        league_keys = list(self.data.keys())
+        sheet = self.wb.sheets['Leagues']
+        sheet['A1'].value = 'LgID'
+        sheet['A2'].options(transpose=True).value = league_keys
+        sheet.autofit()
+
+        headers = ['TmID', 'name', 'shortName', 'tla', 'eloNow']
+        for lg in league_keys:
+            lg_sht = self.wb.sheets[lg]
+            lg_sht.clear_contents()
+            tm_ids = list(self.data[lg]['data'].keys())
+            lg_sht['A1'].value = headers
+            lg_sht['A2'].options(transpose=True).value = tm_ids
+
+            for row, tm in enumerate(tm_ids):
+                tm_data = self.data[lg]['data'][tm]
+                wr_cell = 'B' + str(row + 2)
+                lg_sht[wr_cell].value = [tm_data[headers[1:][i]] for i in range(len(headers[1:]))]
+
+            xw.Range('E2').expand('down').number_format = '0'
+            lg_sht.autofit()
+
+    def write_test(self):
+        sheet = self.wb.sheets['Sandbox']
+        if sheet['A1'].value == 'Hello EloXL!':
+            sheet['A1'].value = 'Bye EloXL!'
+        else:
+            sheet['A1'].value = 'Hello EloXL!'
+        sheet.autofit()
+
+    def __str__(self):
+        return '[Reading from {0} to Excel file {1}]'.format(self.file, self.wb)
+
+    def __repr__(self):
+        return str(self)
 
 
 @xw.func
@@ -18,4 +58,6 @@ def hello(name):
 
 if __name__ == '__main__':
     xw.Book('EloXL.xlsm').set_mock_caller()
-    main()
+    rj = ReadJSON()
+    print(rj)
+    rj.write_leagues()
--- README.md
+++ README.md
@@ -2,27 +2,48 @@
 
 ## Installation
 
+Currenty not set up.
+
 ```bash
     python -m pip install EloXL
 ```
 
 ## Configuration
 
+### Module
+
+Module configuration is in `setup.cfg`.
+
+### XLWings
+
+XLWings configuration is in `xlwings.conf`.
+
 ## Usage
 
-### Input Parameters
+### Updating Data
 
-### Input File Types
+- Running this updates all leagues and overwrites the master `fbd.json` file in the root directory.
+
+```bash
+    python ./app/data/updater_json.py
+```
 
 ### Outputs
 
-#### CSV Files
+#### JSON Files
 
-#### Singular Constants
+The updater scripts in `./app/data/` write output JSON files to the `./app/json/` directory.
+
+- List of updater scripts:
+    - `updater_json.py`: Updates all league data
+    - `write_statics.py`: Updates static JSON files that contain information on leagues and teams.
+    - `updater.py`: Currently obsolete - contains logic for updating HDF5 tables.
 
 ### Excel Integration
 
-#### XLWings
+#### Python: XLWings
+
+The main script for controlling the xlsm file is `EloXL.py`.
 
 ## Development
 
